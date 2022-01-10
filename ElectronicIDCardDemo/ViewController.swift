a9a56e93083742807ffdec1dc7c86f8071ce94f2
HCdouzone <97083034+HCdouzone@users.noreply.github.com>
2022-01-10 13:21:05 +0900

    Initial Commit

---
+++ ElectronicIDCardDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElectronicIDCardDemo
+//
+//  Created by hyengchan on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
