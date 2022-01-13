dc9d7ab7ad333acb6e4613f532e9d175f6672e5a
velmash <hyengchan@gmail.com>
2022-01-13 16:07:54 +0900

    :wrench: 출퇴근 뷰 기능 구현중

---
+++ ElectronicIDCardDemo/Feature/WorkCommuting/ViewController/WorkCommutingViewController+Extension.swift
@@ -0,0 +1,70 @@
+//
+//  WorkCommutingViewController+Extension.swift
+//  ElectronicIDCardDemo
+//
+//  Created by hyengchan on 2022/01/13.
+//
+
+import UIKit
+
+extension WorkCommutingViewController {
+    enum FormatterType {
+        case detail
+        case normal
+    }
+    
+    enum CommuteType: String {
+        case commute = "출근"
+        case leave = "퇴근"
+    }
+    
+    private func getTimeNow(_ type: FormatterType) -> String {
+        let formatter = DateFormatter()
+        if case .detail = type {
+            formatter.dateFormat = "HH:mm:ss"
+        } else {
+            formatter.dateFormat = "HH:mm"
+        }
+        let currentDateString = formatter.string(from: Date())
+        return currentDateString
+    }
+    
+    internal func getDayToday() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko")
+        formatter.dateFormat = "yyyy.MM.dd (EE)"
+        let currentDateString = formatter.string(from: Date())
+        return currentDateString
+    }
+}
+
+
+extension WorkCommutingViewController {
+    internal func showAlarm(_ type: CommuteType) {
+        let alertController = UIAlertController(
+            title: "알림",
+            message: "\(type.rawValue)시간을\n 등록하시겠습니까?",
+            preferredStyle: .alert
+        )
+        let deleteAction = UIAlertAction(title: "확인", style: .default) { _ in
+            self.myView.commuteTime.text = self.getTimeNow(.detail)
+            if case .commute = type {
+                if self.myView.commutingReportView.onTimeDetail.text == "미등록" {
+                    self.myView.commutingReportView.onTimeDetail.text = self.getTimeNow(.normal)
+                } else {
+                    // 토스트뷰 띄우기
+                    print("zz")
+                }
+               
+            } else {
+                self.myView.commutingReportView.offTimeDetail.text = self.getTimeNow(.normal)
+            }
+            
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+}
--- ElectronicIDCardDemo/Feature/WorkCommuting/ViewController/WorkCommutingViewController.swift
+++ ElectronicIDCardDemo/Feature/WorkCommuting/ViewController/WorkCommutingViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import Combine
+import CombineCocoa
 
 class WorkCommutingViewController: BaseViewController<WorkCommutingView> {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // 여기도 서버 통신코드 여기에 구현
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        myView.todayDateView.todayDateLabel.text = getDayToday()
+    }
+    
+    override func pageBinding() {
+        myView.commuteButtonView.commuteButton.tapPublisher
+            .sink { [weak self] in self?.showAlarm(.commute) }
+            .store(in: &bag)
+        
+        myView.commuteButtonView.leaveButton.tapPublisher
+            .sink { [weak self] in self?.showAlarm(.leave) }
+            .store(in: &bag)
+    }
 }
+
